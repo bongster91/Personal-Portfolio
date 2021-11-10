5f329693b23c2f296e7eabc3b5a2e825f3cbabde
Grace Bong <gracebong.park@gmail.com>
2021-11-10 18:59:10 -0500

    app.js added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
+import Navbar from "./Pages/Navbar";
+import MiscHeader from "./Pages/MiscHeader";
+import WelcomeHeader from "./Pages/WelcomeHeader";
+import Bio from "./Pages/Bio";
+import Projects from "./Pages/Projects";
+import Skills from "./Pages/Skills";
+import Experience from "./Pages/Experience";
+import ContactInfo from "./Pages/ContactInfo";
+import RightColumn from "./Pages/RightColumn";
+import styled from "styled-components";
 import './App.css';
+
+const AppContainer = styled.div`
+  font-family: serif;
+  background-color: white;
+  display: grid;
+  grid-template-columns: repeat(12, 1fr);
+  grid-template-rows: repeat(3, 50px) repeat(7, 3fr) 1fr;
+  gap: 20px;
+
+  @media screen and (max-width: 700px) {
+    display:grid;
+    grid-template-columns: repeat(9, 1fr);
+    grid-template-rows: repeat(3, .25fr) repeat(10, 3fr) .25fr;
+  }
+`
+
+const MainSection = styled.main`
+  grid-row: 4/11;
+  grid-column: 1/10;
+  padding: 20px;
+
+  @media screen and (max-width: 700px) {
+    grid-column: 1/11;
+    grid-row: 4/11;
+  }
+`
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppContainer>
+        <Navbar />
+        <MiscHeader />
+        <WelcomeHeader />
+
+        <MainSection>
+          <Bio />
+          <Projects />
+          <Skills />
+          <Experience />
+        </MainSection>
+
+        <ContactInfo />
+        <RightColumn />
+    </AppContainer>
   );
-}
+};
 
 export default App;
