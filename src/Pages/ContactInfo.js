0b3c133493a4bdf9ac2fc8b1fc4e60a786a0e059
Grace Bong <gracebong.park@gmail.com>
2021-11-10 19:03:52 -0500

    contact info section complete

---
+++ src/Pages/ContactInfo.js
@@ -0,0 +1,41 @@
+import styled from "styled-components";
+
+const ContactContainer = styled.section`
+    border-top: 3px solid black;
+    grid-row: 11/12;
+    grid-column: 1/13;
+
+    h2 {
+        font-style: italic;
+        border-bottom: 1px solid black;
+        padding-left: 10px;
+    }
+
+    a {
+        padding-left: 15px;
+        font-weight: bold;
+        color: #0000FF;   
+    }
+
+    a:hover {
+        color: darkgoldenrod;
+    }
+
+    @media screen and (max-width: 700px) {
+        grid-row: 9;
+        grid-column: 1/13;
+    }
+`
+
+function ContactInfo() {
+    return (
+        <ContactContainer id="contact">
+            <h2>Contact Me</h2>
+            <a className="github" href="https://github.com/bongster91">GitHub</a>
+            <a className="linkedin" href="https://www.linkedin.com/in/grace-bong-7922b7202/">LinkedIn</a>
+            <a className="email" href="mailto:gracebong.park@gmail.com">Email</a>
+        </ContactContainer>
+    );
+};
+
+export default ContactInfo;
