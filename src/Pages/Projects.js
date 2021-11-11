bc6b90477da4feb7d514aa6567efd44f87475770
Grace Bong <gracebong.park@gmail.com>
2021-11-10 19:05:41 -0500

    projects section complete

---
+++ src/Pages/Projects.js
@@ -0,0 +1,57 @@
+import textBasedAdventure from '../assets/Text based Adventure Giphy.gif';
+import capstone from '../assets/capstone-giphy.gif';
+import styled from 'styled-components';
+
+const ProjectContainer = styled.section`
+    border-bottom: 2px solid black;
+    grid-row: 6/9;
+
+    h2 {
+        font-style: italic;
+        border-bottom: 1px solid black;
+    }
+
+    .project-giphy {
+        max-width: 100%;
+        height: 400px;
+        width: 800px;
+        padding: 5px;
+    }
+
+    a {
+        color: #0000FF;
+        font-size: 18px;
+        font-weight: bold;
+    }
+
+    a:hover {
+        color: darkgoldenrod;
+    }
+`
+
+function Projects() {
+    return (
+        <ProjectContainer id="projects">
+            <h2>Projects</h2>
+            <span>{console.log(ProjectContainer)}
+                <a href="https://github.com/kathypurry/capstonegang">Tech Type</a>
+                <p>Typing improvement game for developers.</p>
+                <img className="project-giphy" src={capstone} alt="project-giphy" />
+            </span>
+
+            <span>
+                <a href="https://github.com/bongster91/coffee-project">JavaScript Coffee</a>
+                <p>Coffee from developers, for developers.</p>
+            </span>
+
+            <span>
+                <a href="https://github.com/bongster91/FSW-Text-Based-Adventure">Text Based Adventure</a>
+                <p>Text based, turn based, multiple ending story on terminal.</p>
+                <img className="project-giphy"  src={textBasedAdventure} alt="project-giphy" />
+            </span>
+
+        </ProjectContainer>
+    );
+};
+
+export default Projects;
