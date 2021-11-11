c374688a2c1cbbfe5c7713e195f3b6b1730de257
Grace Bong <gracebong.park@gmail.com>
2021-11-10 19:12:44 -0500

    possible dad joke api

---
+++ src/Components/RightSideMiscAPI.js
@@ -0,0 +1,30 @@
+//import axios from 'axios';
+import { useState } from 'react';
+
+function RightSideMiscAPI() {
+    const [ joke ] = useState();
+    
+    // const handleDadJoke = async() => {
+        
+    //         await axios
+    //             .get(`https://icanhazdadjoke.com/`)
+    //             .then(
+    //                 (res) => {
+    //                     setJoke(res.data)
+    //                 },
+    //                 (error) => console.log('get', error)
+    //             )
+    //             .then((res) => console.log(res))
+    //             .catch((c) => console.warn('catch', c));
+        
+    // }
+
+    return (
+        <div className="right-column-api">
+            <button className="dad-joke">Click here for a dad joke!</button>
+            <p>{joke}</p>
+        </div>
+    );
+};
+
+export default RightSideMiscAPI;
